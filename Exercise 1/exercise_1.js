d2a665e926ef5247c6429c0fadcd09ed7742d0df
ranaramisty <mistyclairer@yahoo.com>
2019-01-11 17:34:32 +0800

    Exercises.

---
+++ Exercise 1/exercise_1.js
@@ -0,0 +1,14 @@
+// let score;
+
+// submitBtn.onclick=()=>{
+//     let num1=document.getElementsByName("q1");
+
+//     if(num1[3].checked){
+//         return score+=1;
+//     }
+//     alert(score);
+// }
+
+
+
+
--- exercise_2.js
+++
@@ -1,8 +0,0 @@
-let test=(x)=>{
-    while(n<5){
-        n++;
-    }
-    return n;
-}
-
-alert(test(5))
